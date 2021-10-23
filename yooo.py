d2b51858ea6fc606107d61ef1ee40464a72266d5
ratonotota <ratonotota@gmail.com>
2021-10-23 18:36:02 -0300

    Update yooo.py

--- yooo.py
+++ yooo.py
@@ -1,28 +1,35 @@
-#ask if you want to add to a list or recive fromt he list
-#if add, call a function that adds your input to a list
-#if recive, print a random item from the list using listx[rnd.randrange(int(len(listx))-1, int(len(listx)))]
-import random as rng
-x = True
-lista = [] #cum, yes, no
+import random as rnd
+x = True    
+response = ""
+response2 = ""
 
-def cum(nigger):
-    lista.append(nigger)
+def rolldice(guess):
+    numb = rnd.randrange(1,6)
+    print("the dice rolled " +  str(numb) + " while you guessed " + str(guess))
 
-def cum2():
-    print(lista[rng.randrange(int(len(lista))-1, int(len(lista)))])
 
-    
 
 while x == True:
-    print("Do you want to add or receive the item?")
+    print("Want to roll again?")
     response = input()
-    if (response == "add"):
-        print("You little bitch what do u want to add papa?")
-        pig = input()
-        cum(pig)
-    elif (response == "receive"):
-        cum2()
-        x = False
+    if (response == "yes"):
+        print("guess the number : ")    
+        guessN = input()
+        rolldice(int(guessN))
+    else:
+        print("bye then")
+        x == False
+
+
+        
+
+         
+
+
+        
+
+
+
 
 
 
